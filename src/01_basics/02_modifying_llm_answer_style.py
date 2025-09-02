a0cb43303e7a5cec6d43e20ccb8d03a9450efd1e
Vinícius Azevedo <sousa0240@gmail.com>
2025-09-01 21:39:25 -0300

    wip: showing changes in trip agent

--- src/01_basics/02_modifying_llm_answer_style.py
+++ src/01_basics/02_modifying_llm_answer_style.py
@@ -1,3 +1,5 @@
+# This is an example about how we can dinamically change the context provided in chains
+
 from ..setup import *
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -16,11 +18,3 @@ prompt = ChatPromptTemplate.from_messages([
 
 for context in contexts:
     print(llm_chain(prompt).invoke({"input": "What do you like to do?", "context": context}), end=divisor)
-
-# Here we had the same idea as the 01_hello_chain file, but now we add the context parameter to the chain,
-# which is a game changer for the way the LLM answer. Run this file to see how different the outputs are!
-
-# We can define as many parameters for the chains as we want. The only thing we must do is to have all the parameters
-# that the chain is going to use set before it operates
-
-# There are ways in which we can add more parameters dinamically (i.e. while the chain is running), I'll show this feature later on
--- src/01_basics/03_trip_agent.py
+++ src/01_basics/03_trip_agent.py
@@ -1,40 +1,62 @@
+# This is a simple trip agent, which:
+# 1. Provides cities to go for the user
+# 2. Picks a random city from the output
+# 3. Tell some places to go on that city
+# 4. Think about how long it would be to visit some of these places on topic 3
+
 from ..setup import *
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import *
-from random import choice
+from random import choice, randint, sample
 
-places2go_sys_message = """
+def remove_random_elements(lst):
+    """
+    Removes random elements from a list and returns a new list.
+    """
+    count = randint(0, len(lst))
+
+    indices_to_remove = set(sample(range(len(lst)), count))
+    return [item for i, item in enumerate(lst) if i not in indices_to_remove]
+
+places2go_sys_msg = """
 You must suggest cities in different continents.
 You must answer only the name of the cities separated by comma.
 """
 
 places2go_prompt = ChatPromptTemplate.from_messages([
-    ("system", places2go_sys_message),
+    ("system", places2go_sys_msg),
     ("user", "{input}"),
 ])
 
-itinerary_sys_message = """
+itinerary_sys_msg = """
 You must answer only places to go separated by comma.
 """
 
 itinerary_prompt = ChatPromptTemplate.from_messages([
-    ("system", itinerary_sys_message),
+    ("system", itinerary_sys_msg),
     ("user", "Give me an itinerary for this city you have mentioned: {itinerary_city}"),
 ])
 
-trip_price_sys_message = """
-You must answer the price in US Dollars, only.
+trip_duration_sys_msg = """
+You only must say the total amount of hours necessary to visit all the places mentioned.
 """
 
-trip_price_prompt = ChatPromptTemplate.from_messages([
-    ("system", trip_price_sys_message),
-    ("user", "{trip_price_input}"),
+trip_duration_prompt = ChatPromptTemplate.from_messages([
+    ("system", trip_duration_sys_msg),
+    ("user", "How long would it be to visit these places you have mentioned? {trip_duration_input}"),
 ])
 
 main_chain = (
       llm_chain(places2go_prompt)
-    | RunnableAssign({'itinerary_city': lambda state: choice(state['answer'].split(", "))})
-    | chain_value_print
+    | show_chain_data
+    | {'itinerary_city': lambda x: choice(x['answer'].split(", "))}
+    | show_chain_data
+    | llm_chain(itinerary_prompt)
+    | show_chain_data
+    | {'trip_duration_input': lambda x: remove_random_elements(x['answer'].split(", "))}
+    | show_chain_data
+    | llm_chain(trip_duration_prompt)
+    | show_chain_data
 )
 
 main_chain.invoke({"input": "tell me places to visit"})
