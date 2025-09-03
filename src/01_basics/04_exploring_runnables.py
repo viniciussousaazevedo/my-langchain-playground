9610185d986e745dc894bccb4594576f62bed3c5
Vinícius Azevedo <sousa0240@gmail.com>
2025-09-03 13:37:44 -0300

    starting from scratch

--- src/01_basics/04_exploring_runnables.py
+++ src/01_basics/04_exploring_runnables.py
@@ -5,14 +5,12 @@
 # 3. Classifies the text's mood (happy, neutral, sad).
 # 4. Observes the text's length, determining which model will respond based on it.
 # 5. Creates a personalized message based on the mood.
-# 6. Formats the output as a JSON object.
 
 from ..llm import *
 
 def normalize(text: str) -> str:
-    # Simple normalization function)
+    # Simple normalization function
     return " ".join(text.strip().split()).lower()
-normalize_runnable = RunnableAssign({'input': lambda x: normalize(x['input'])})
 
 mood_classifier_prompt = ChatPromptTemplate.from_messages([
     ('system', "you must classify the user input in only one word contained in these options: {options}. You must also declare your model name at the end like the example provided, in parenthesis (e.g. Llama, GPT, Mistral, etc)."),
@@ -26,12 +24,5 @@ model_selector = RunnableBranch(
     lambda x: LLM(),
 )
 
-main_chain = (
-    normalize_runnable
-    | show_chain_data
-    | RunnableAssign({'model': model_selector})
-    | show_chain_data
-    | RunnableLambda(lambda x: x['model'].get_chain(mood_classifier_prompt))
-    | show_chain_data
-)
-main_chain.invoke({'options': ['sad', 'angry', 'happy', 'confused'], 'input': "I   just got promoted in my job!"})
+
+
