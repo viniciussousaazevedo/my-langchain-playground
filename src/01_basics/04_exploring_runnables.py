5fe0382ad57fb7b799e8d900cac70480c2e2e0a9
Vinícius Azevedo <sousa0240@gmail.com>
2025-09-02 21:38:54 -0300

    wip: file 01.04

--- src/01_basics/04_exploring_runnables.py
+++ src/01_basics/04_exploring_runnables.py
@@ -8,14 +8,29 @@
 # 6. Formats the output as a JSON object.
 
 from ..llm import *
+limited_model = LLM("llama-3.1-8b-instant")
 
 def normalize(text: str) -> str:
     # Simple normalization function
     return ' '.join(text.strip().split()).lower()
+normalize_runnable = RunnableLambda(lambda x: {"input": normalize(x["input"]), **x})
 
 mood_classifier_prompt = ChatPromptTemplate.from_messages([
     ("system", "you must classify the user input in only one word contained in these options: {options}"),
     ("user", "{input}")
 ])
 
+llm_instance = RunnableBranch(
+    (lambda x: len(x["input"].split()) < 10, limited_model),
+    llm
+)
 
+main_chain = (
+    normalize_runnable
+    mood_classifier_prompt
+    | show_chain_data
+    | # TODO
+    
+)
+
+main_chain.invoke({"options": ['sad', 'angry', 'happy', 'confused'], "input": "I just got promoted in my job!"})
--- src/llm.py
+++ src/llm.py
@@ -2,6 +2,7 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import *
+from operator import itemgetter
 
 class LLM:
     def __init__(self, name="openai/gpt-oss-120b"):
