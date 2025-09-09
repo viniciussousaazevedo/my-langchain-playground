a0423f027634e73253a34d59342ed140e3765e0e
Vinícius Azevedo <sousa0240@gmail.com>
2025-09-09 11:29:40 -0300

    finish 03.02

--- src/03_rag/02_embeddings.py
+++ src/03_rag/02_embeddings.py
@@ -1,5 +1,9 @@
+
+# This does not use any vector store for now, comparing cosine similarity manually
 # This file shows a simple query between vectorized data and queries using an embedding model to find the most relevant samples.
 from ..util import *
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 data = [
     "I can't believe they did this to Gustave!",
@@ -10,7 +14,14 @@ data = [
 
 query = "Who is Verso's sister?"
 
-embedded_data = [embedding_model.embed(d) for d in data]
-embedded_query = embedding_model.embed(query)
+embedded_data = np.array([embedding_model.embed(d) for d in data])
+embedded_query = np.array(embedding_model.embed(query)).reshape(1, -1)
 
+scores = cosine_similarity(embedded_query, embedded_data)[0]
 
+ranked_results = sorted(zip(data, scores), key=lambda x: x[1], reverse=True)
+
+print("Query:", query)
+print("\nTop results:")
+for text, score in ranked_results:
+    print(f"({score:.4f}) {text}")
