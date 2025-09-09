9ba4ae9455d9cfe0b4c8877f1bd7be0dbad44f6f
Vinícius Azevedo <sousa0240@gmail.com>
2025-09-09 11:57:52 -0300

    idealization of 03.03

--- src/03_rag/03_vectorstores.py
+++ src/03_rag/03_vectorstores.py
@@ -1,1 +1,4 @@
-# TODO
+# This file will perform just like the 02.01 file (running states),
+# but this version will use embeddings with FAISS vector store to retrieve
+# and update the Knowledge Base Pydantic object
+
